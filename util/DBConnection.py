bce318e751469312ef116a46af546429da6d8fd1
pratikwani116@gmail.com <pratikwani116@gmail.com>
2023-12-18 13:31:52 +0530

    commit

--- util/DBConnection.py
+++ util/DBConnection.py
@@ -3,8 +3,8 @@ import util.PropertyUtil as PropertyUtil
 class DBConnection:
     def open(self):
         try:
-            # conn=PropertyUtil.getPropertyString()
-            self.connection=sql.connect(host="localhost",database="HospitalManagement",username="root",password="JEEjee@116")
+            conn=PropertyUtil.getPropertyString()
+            self.connection=sql.connect(host=conn[1],database=conn[2],username=conn[0],password=conn[3])
             self.stmt=self.connection.cursor()
             if self.connection.is_connected:
                 print("***Database Connected***")
